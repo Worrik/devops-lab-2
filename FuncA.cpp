e8b1db1feb69cf08919047d761fa36d5a2796ba0
Volodymyr <vovachornand@gmail.com>
2024-11-27 17:49:58 +0200

    initial commit

---
+++ FuncA.cpp
@@ -0,0 +1,5 @@
+#include "FuncA.h"
+
+double TrigonometricFunction::FuncA() {
+    return 0.0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    TrigonometricFunction func;
+    std::cout << func.FuncA() << std::endl;
+    return 0;
+}
